cbaa71237f5ced922fa6225f951ea1974b8791f3
Piotr Adamski <piotr@adamscy.net>
2022-09-23 22:47:35 +0200

    readability page

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addShortcode("page", function(name) {
+    return `<a href="pages/${name}">${name}</a>`;
+  });
+  eleventyConfig.addPassthroughCopy('css');
   return {
     dir: {
       input: "views",
       output: "dist",
       layouts: "layouts"
-    }
+    },
+    passthroughFileCopy: true
   }
 }
